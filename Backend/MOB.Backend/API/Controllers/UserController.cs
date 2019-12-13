6b764a45a092b486c5819e8373e345f02124c293
Lautaro Marchetti <LaMarche@moravia.com>
2019-12-13 14:47:19 -0300

    Inital commit

---
+++ Backend/MOB.Backend/API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+                
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Index()
+        {
+            return new List<User>()
+            {
+                new User(){Name = "Seba", Age = 24}
+            };
+        }
+    }
+}
