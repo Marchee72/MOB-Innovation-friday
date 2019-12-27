1a6a43587b1d8de380048cebfbc15d94cada7d21
Lautaro Marchetti <LaMarche@moravia.com>
2019-12-27 16:07:45 -0300

    nre view with vuetify

---
+++ fronted-vuetify/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <MainCalendar/>
+  </v-container>
+</template>
+
+<script lang="ts">
+import {Vue,  Component}  from "vue-property-decorator";
+import MainCalendar from "./MainCalendar.vue";
+
+@Component({
+  components: {MainCalendar}
+})
+export default class HelloWorld extends Vue {
+
+}
+</script>
---
+++ fronted-vuetify/src/components/MainCalendar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-sheet height="500">
+      <v-calendar type="week" now="2019-01-08" value="2019-01-08"></v-calendar>
+    </v-sheet>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class MainDashboard extends Vue {
+
+}
+</script>
+
+<style>
+</style>
